f05499f2439ab6c30c0aa7e437bbf9ddb54259f8
Rodrigues <fr.costa95@gmail.com>
2021-03-08 18:21:50 -0300

    feat(header): improve header, include mat-icons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,12 @@ import { HeaderComponent } from './components/header/header.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatButtonModule
   ],
   exports: [
     HeaderComponent,
